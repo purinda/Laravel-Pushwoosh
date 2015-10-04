a69acc93ec47865f23f2605d32229dea00eb9fe5
Vincent Klaiber <vincentklaiber@gmail.com>
2015-10-04 14:56:45 +0200

    Improved Lumen support

--- src/PushwooshServiceProvider.php
+++ src/PushwooshServiceProvider.php
@@ -29,20 +29,24 @@ class PushwooshServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->setupConfig();
+        $this->setupConfig($this->app);
     }
 
     /**
      * Setup the config.
      *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     *
      * @return void
      */
-    protected function setupConfig()
+    protected function setupConfig(Application $app)
     {
         $source = realpath(__DIR__.'/../config/pushwoosh.php');
 
         if (class_exists('Illuminate\Foundation\Application', false)) {
             $this->publishes([$source => config_path('pushwoosh.php')]);
+        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+            $app->configure('pushwoosh');
         }
 
         $this->mergeConfigFrom($source, 'pushwoosh');
