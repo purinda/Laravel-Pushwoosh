6aa30a29e9e37997f5b6b0d8d7aa2469366a5c88
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-13 09:21:12 +0100

    Improved environment specific code

--- src/PushwooshServiceProvider.php
+++ src/PushwooshServiceProvider.php
@@ -13,7 +13,9 @@ namespace Schimpanz\Pushwoosh;
 
 use Gomoob\Pushwoosh\Client\Pushwoosh;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * This is the Pushwoosh service provider class.
@@ -43,9 +45,9 @@ class PushwooshServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/pushwoosh.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('pushwoosh.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('pushwoosh');
         }
 
